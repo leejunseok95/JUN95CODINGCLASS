cc7169067ed76e4db03f71b48c3ce4240f31564c
leejunseok95 <77767132+leejunseok95@users.noreply.github.com>
2021-07-09 18:55:40 +0900

    Delete NotConfirmException.java

--- jun_Class/NotConfirmException.java
+++
@@ -1,9 +0,0 @@
-package leveltest2;
-
-@SuppressWarnings("serial")
-public class NotConfirmException extends Exception {
-	@Override
-	public String toString() {
-		return "본인이 아닙니다. 담임 혹은 본인만 확인할 수 있습니다.";
-	}
-}
